d8dfd8a0a1ec3420566d564bfaff1bbe46272258
Cryptic-shubham <162601751+Cryptic-shubham@users.noreply.github.com>
2024-03-07 21:35:49 +0530

    Update and rename omni_yunluo.mk to twrp_yunluo.mk

--- twrp_yunluo.mk
+++ twrp_yunluo.mk
@@ -7,16 +7,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from yunluo device
 $(call inherit-product, device/xiaomi/yunluo/device.mk)
 
 PRODUCT_DEVICE := yunluo
-PRODUCT_NAME := omni_yunluo
+PRODUCT_NAME := twrp_yunluo
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22081283G
 PRODUCT_MANUFACTURER := xiaomi
