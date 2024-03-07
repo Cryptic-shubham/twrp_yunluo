553ba1d53dab288ac7d2533079a00816c6e30d0a
Cryptic-shubham <162601751+Cryptic-shubham@users.noreply.github.com>
2024-03-07 20:45:15 +0530

    Remove unneeded lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_yunluo.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_yunluo-user \
-    twrp_yunluo-userdebug \
-    twrp_yunluo-eng
+COMMON_LUNCH_CHOICES := twrp_yunluo-eng
